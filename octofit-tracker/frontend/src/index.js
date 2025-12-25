484477cb4da1aa0324822773dc4267545acc0180
raunaksingh1234 <raunak.singh@accenture.com>
2025-12-25 05:13:42 +0000

    Fix: React frontend and Django backend launch issues (imports, manage.py syntax)

--- octofit-tracker/frontend/src/index.js
+++ octofit-tracker/frontend/src/index.js
@@ -1,4 +1,7 @@
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import reportWebVitals from './reportWebVitals';
 
 // Log the codespace environment variable for debugging
 console.log('REACT_APP_CODESPACE_NAME:', process.env.REACT_APP_CODESPACE_NAME);
